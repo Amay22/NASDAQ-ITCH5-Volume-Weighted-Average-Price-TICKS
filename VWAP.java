d5b77254270b7a6d0cd97b16e885b40681ad412d
amay22 <amay@nyu.edu>
2015-02-21 16:50:00 -0500

    Final

--- VWAP.java
+++ VWAP.java
@@ -13,57 +13,121 @@ import java.util.Iterator;
 import java.util.Map;
 public class VWAP {
     public static class VWAPData {
+
         double price;
         int volume;
+
         public VWAPData(double price, int volume) {
             this.price = price;
             this.volume = volume;
         }
+
         @Override
         public String toString() {
             return (this.price + " " + this.volume);
         }
     }
+    public static class VWAPDataPrice {
+
+        double price;
+        String name;
+
+        public VWAPDataPrice(double price, String name) {
+            this.price = price;
+            this.name = name;
+        }
+    }
     static HashMap<String, ArrayList<VWAPData>> C = new HashMap<>();
     public static void main(String args[]) throws IOException, InterruptedException {
-        HashMap<Long, String> A = new HashMap<>();
+        HashMap<Long, VWAPDataPrice> A = new HashMap<>();
         InputStream input = new FileInputStream(new File("06022014.NASDAQ_ITCH50"));
+        int count = 0;
         while (input.read() != -1) {
+            if(count % 50000000 ==0) {System.out.println(C.toString());}count++;
             int payLength = input.read();
             byte[] payBytes = new byte[payLength];
             input.read(payBytes);
             String str = new String(payBytes);
             switch (str.charAt(0)) {
                 case 'A':
-                    if ((char) payBytes[19] == 'B') {
-                        A.put((ByteBuffer.wrap(Arrays.copyOfRange(payBytes, 11, 19)).getLong()), 
-                            "" + ((char) payBytes[24]) + "" + ((char) payBytes[25]) 
-                            + "" + ((char) payBytes[26]) + "" + ((char) payBytes[27]) 
+                case 'F':
+                    if (("" + ((char) payBytes[24]) + "" + ((char) payBytes[25])
+                            + "" + ((char) payBytes[26]) + "" + ((char) payBytes[27])
                             + "" + ((char) payBytes[28]) + "" + ((char) payBytes[29])
                             + "" + ((char) payBytes[30]) + "" + ((char) payBytes[31])
-                            + "" + ((char) payBytes[32]));
+                            + "" + ((char) payBytes[32])).trim().equals("CXO")) {
+                        if ((char) payBytes[19] == 'B') {
+                            A.put((ByteBuffer.wrap(Arrays.copyOfRange(payBytes, 11, 19)).getLong()),
+                                    new VWAPDataPrice(((ByteBuffer.wrap(Arrays.copyOfRange(payBytes, 32, 36)).getInt()) / 10000),
+                                            "" + ((char) payBytes[24]) + "" + ((char) payBytes[25])
+                                            + "" + ((char) payBytes[26]) + "" + ((char) payBytes[27])
+                                            + "" + ((char) payBytes[28]) + "" + ((char) payBytes[29])
+                                            + "" + ((char) payBytes[30]) + "" + ((char) payBytes[31])
+                                            + "" + ((char) payBytes[32])));
+                        }
                     }
                     break;
-                case 'C':
-                    if ((char) payBytes[31] == 'Y' && A.get((ByteBuffer.wrap(Arrays.copyOfRange(payBytes, 11, 19)).getLong())) != null) {
-                        if (C.get(A.get((ByteBuffer.wrap(Arrays.copyOfRange(payBytes, 11, 19)).getLong()))) == null) {
-                            ArrayList<VWAPData> temp = new ArrayList<>();
-                            temp.add(new VWAPData(((ByteBuffer.wrap(Arrays.copyOfRange(payBytes, 32, 36)).getInt()) / 10000),
-                                    (ByteBuffer.wrap(Arrays.copyOfRange(payBytes, 19, 23)).getInt())));
-                            C.put(A.get((ByteBuffer.wrap(Arrays.copyOfRange(payBytes, 11, 19)).getLong())), temp);
+                case 'U':
+                    if (A.get((ByteBuffer.wrap(Arrays.copyOfRange(payBytes, 11, 19)).getLong())) != null) {
+                        A.put((ByteBuffer.wrap(Arrays.copyOfRange(payBytes, 19, 27)).getLong()),
+                                new VWAPDataPrice(((ByteBuffer.wrap(Arrays.copyOfRange(payBytes, 31, 35)).getInt()) / 10000),
+                                        A.get((ByteBuffer.wrap(Arrays.copyOfRange(payBytes, 11, 19)).getLong())).name));
+                        A.remove(ByteBuffer.wrap(Arrays.copyOfRange(payBytes, 11, 19)).getLong());
+                    }
+                    break;
+                case 'E':
+                    if (A.get((ByteBuffer.wrap(Arrays.copyOfRange(payBytes, 11, 19)).getLong())) != null) {// && (char) payBytes[31] == 'Y') {
+                        ArrayList<VWAPData> temp1;
+                        if (C.get(A.get((ByteBuffer.wrap(Arrays.copyOfRange(payBytes, 11, 19)).getLong())).name) == null) {
+                            temp1 = new ArrayList<>();
                         } else {
-                            ArrayList<VWAPData> temp = C.get(A.get((ByteBuffer.wrap(Arrays.copyOfRange(payBytes, 11, 19)).getLong())));
-                            temp.add(new VWAPData(
-                                    (((double) (ByteBuffer.wrap(Arrays.copyOfRange(payBytes, 32, 36)).getInt())) / 10000),
-                                    (ByteBuffer.wrap(Arrays.copyOfRange(payBytes, 19, 23)).getInt())));
-                            C.put(A.get((ByteBuffer.wrap(Arrays.copyOfRange(payBytes, 11, 19)).getLong())), temp);
+                            temp1 = C.get(A.get((ByteBuffer.wrap(Arrays.copyOfRange(payBytes, 11, 19)).getLong())).name);
                         }
+                        temp1.add(new VWAPData(A.get((ByteBuffer.wrap(Arrays.copyOfRange(payBytes, 11, 19)).getLong())).price,
+                                (ByteBuffer.wrap(Arrays.copyOfRange(payBytes, 19, 23)).getInt())));
+                        C.put(A.get((ByteBuffer.wrap(Arrays.copyOfRange(payBytes, 11, 19)).getLong())).name, temp1);
                         A.remove((ByteBuffer.wrap(Arrays.copyOfRange(payBytes, 11, 19)).getLong()));
+                    }                    
+                    break;
+                case 'C':
+                     if (A.get((ByteBuffer.wrap(Arrays.copyOfRange(payBytes, 11, 19)).getLong())) != null && (char) payBytes[31] == 'Y') {
+                        ArrayList<VWAPData> temp;
+                        if (C.get(A.get((ByteBuffer.wrap(Arrays.copyOfRange(payBytes, 11, 19)).getLong())).name) == null) {
+                            temp = new ArrayList<>();
+                        } else {
+                            temp = C.get(A.get((ByteBuffer.wrap(Arrays.copyOfRange(payBytes, 11, 19)).getLong())).name);
+                        }
+                        temp.add(new VWAPData(((ByteBuffer.wrap(Arrays.copyOfRange(payBytes, 32, 36)).getInt()) / 10000),
+                                (ByteBuffer.wrap(Arrays.copyOfRange(payBytes, 19, 23)).getInt())));
+                        C.put(A.get((ByteBuffer.wrap(Arrays.copyOfRange(payBytes, 11, 19)).getLong())).name, temp);
+                        A.remove((ByteBuffer.wrap(Arrays.copyOfRange(payBytes, 11, 19)).getLong()));
+                    }                   
+                    break;
+                case 'P':
+                    if (("" + ((char) payBytes[24]) + "" + ((char) payBytes[25])
+                            + "" + ((char) payBytes[26]) + "" + ((char) payBytes[27])
+                            + "" + ((char) payBytes[28]) + "" + ((char) payBytes[29])
+                            + "" + ((char) payBytes[30]) + "" + ((char) payBytes[31])
+                            + "" + ((char) payBytes[32])).trim().equals("CXO")) {
+                    ArrayList<VWAPData> temp;                    
+                    String stock_name = "" + ((char) payBytes[24]) + "" + ((char) payBytes[25])
+                            + "" + ((char) payBytes[26]) + "" + ((char) payBytes[27])
+                            + "" + ((char) payBytes[28]) + "" + ((char) payBytes[29])
+                            + "" + ((char) payBytes[30]) + "" + ((char) payBytes[31])
+                            + "" + ((char) payBytes[32]);
+                    if (C.get(stock_name) != null) {
+                        temp = C.get(stock_name);
+                    } else {
+                        temp = new ArrayList<>();
+                    }
+                    temp.add(new VWAPData(
+                            (((double) (ByteBuffer.wrap(Arrays.copyOfRange(payBytes, 32, 36)).getInt())) / 10000),
+                            (ByteBuffer.wrap(Arrays.copyOfRange(payBytes, 20, 24)).getInt())));
+                    C.put(stock_name, temp);                    
                     }
                     break;
                 case 'X':
                 case 'D':
-                case 'U':
                     A.remove((ByteBuffer.wrap(Arrays.copyOfRange(payBytes, 11, 19)).getLong()));
                     break;
             }
@@ -98,6 +162,6 @@ public class VWAP {
             bw.write(output);
         }
         Desktop.getDesktop().open(new File("test.htm"));
-        System.out.println("Done");
+        System.out.println(count+" Done");
     }
 }
